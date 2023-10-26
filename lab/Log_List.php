27ff0e8ad20aac81835b523bedee20cc21cfd2e8
Malanott <144749660+Malanott@users.noreply.github.com>
2023-10-26 15:38:20 +0800

    Add files via upload

---
+++ lab/Log_List.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratory Logging System - Log List</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Log List</h1>
+    <table>
+        <tr>
+            <th>Log ID</th>
+            <th>Student ID</th>
+            <th>Class ID</th>
+            <th>Log Date</th>
+            <th>PC Number</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>ST123</td>
+            <td>CS101</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC001</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>ST124</td>
+            <td>CS102</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC002</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>ST125</td>
+            <td>CS103</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC003</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>ST126</td>
+            <td>CS104</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC004</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+         <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+                 <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+                 <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+                 <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+                 <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+                 <tr>
+            <td>5</td>
+            <td>ST127</td>
+            <td>CS105</td>
+            <td>2023-01-15 08:30:00</td>
+            <td>PC005</td>
+        </tr>
+        
+    </table>
+</body>
+</html>
